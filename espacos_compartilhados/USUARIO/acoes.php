acf7fae2332bd6584e481c91aa3805caf1e2e0a8
mariaaa <toledomiranda.maria23@gmail.com>
2025-02-05 15:07:58 -0400

    área usuário finalizada

--- espacos_compartilhados/USUARIO/acoes.php
+++ espacos_compartilhados/USUARIO/acoes.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:espacos_compartilhados/ACOES/acoes.php
 <?php
     session_start();
     require '../DB/conexao.php';
@@ -63,32 +62,5 @@
             exit;
         }
     }
-    
-=======
-<?php
-    session_start();
-    require './DB/conexao.php';
 
-    if(isset($_POST['create_usuario'])){
-        $nome = mysqli_real_escape_string($conexao, trim($_POST['nome_usuario']));
-        $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
-        $telefone = mysqli_real_escape_string($conexao, trim($_POST['telefone']));
-        $cpf = mysqli_real_escape_string($conexao, trim($_POST['cpf']));
-
-        $sql = "INSERT INTO usuario (nome_usuario, email, telefone, cpf) VALUES ('$nome', '$email', '$telefone', '$cpf')";
-
-        mysqli_query($conexao, $sql);
-
-        if(mysqli_affected_rows($conexao) > 0){
-            $_SESSION['mensagem'] = 'Usuário Criado com Sucesso!';
-            header('Location: ./USUARIO/usuario-index.php');
-            exit;
-        } else{
-            $_SESSION['mensagem'] = 'Erro ao Criar Usuário!';
-            header('Location: ./USUARIO/usuario-index.php');
-            exit;
-        }
-    }
-    
->>>>>>> b3bc3df894ab6cade596db5f3137b1c214b2027a:espacos_compartilhados/USUARIO/acoes.php
 ?>
